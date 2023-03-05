8848f26b749386973bc4df0d128151b19e4d17ab
Muzamil Pirzada <muzmailali34@gmail.com>
2023-03-05 20:51:56 +0500

    login screen changes

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -24,95 +24,99 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Center(
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white, width: 2),
-                    shape: BoxShape.circle),
-                child: Center(
-                    child: Icon(
-                  Icons.person,
-                  color: Colors.white,
-                  size: 120,
-                )),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              TextField(
-                style: TextStyle(color: Colors.white),
-                controller: usernameController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(18),
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  hintText: "Username",
-                  hintStyle: TextStyle(color: Colors.white),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(18),
-                    borderSide: const BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(18),
-                    borderSide: const BorderSide(color: Colors.white),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white, width: 2),
+                      shape: BoxShape.circle),
+                  child: Center(
+                      child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 120,
+                  )),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                TextField(
+                  style: TextStyle(color: Colors.white),
+                  controller: usernameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    hintText: "Username",
+                    hintStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                style: const TextStyle(color: Colors.white),
-                controller: passwordController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(18),
-                    borderSide: const BorderSide(color: Colors.white),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  style: const TextStyle(color: Colors.white),
+                  controller: passwordController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    hintText: "Password",
+                    hintStyle: const TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
                   ),
-                  hintText: "Password",
-                  hintStyle: const TextStyle(color: Colors.white),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(18),
-                    borderSide: const BorderSide(color: Colors.white),
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      "Sign in",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20),
+                    ),
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(18),
-                    borderSide: const BorderSide(color: Colors.white),
+                  style: ElevatedButton.styleFrom(
+                    shape: StadiumBorder(),
+                    primary: Color.fromARGB(255, 228, 226, 226),
+                    onPrimary: Colors.blue,
+                    padding: EdgeInsets.symmetric(vertical: 16),
                   ),
                 ),
-                obscureText: true,
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    "Sign in",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20),
-                  ),
+                SizedBox(
+                  height: 20,
                 ),
-                style: ElevatedButton.styleFrom(
-                  shape: StadiumBorder(),
-                  primary: Color.fromARGB(255, 228, 226, 226),
-                  onPrimary: Colors.blue,
-                  padding: EdgeInsets.symmetric(vertical: 16),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Can' 't acces to your account?',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white),
-              )
-            ],
+                Text(
+                  'Can' 't acces to your account?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                )
+              ],
+            ),
           ),
         ),
       ),
